1d1ed34b844d45c300b77566da97c21e04889bec
Avishake2017 <avishakedutta@gmail.com>
2019-08-11 15:34:35 +0530

    sixth commit

--- src/main/java/daos/CategoryDaoimpl.java
+++ src/main/java/daos/CategoryDaoimpl.java
@@ -2,17 +2,41 @@ package daos;
 
 import java.util.List;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import model.Category;
-
+@Repository(value = "CategoryDao")
+@Transactional
 public class CategoryDaoimpl implements CategoryDao{
+	@Autowired
 
+	private SessionFactory sessionFactory;
 	public boolean addCategory(Category category) {
-		
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			session.save(category);
+			return true;
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	public List<Category> getAllCategory() {
-		
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			Query query = session.createQuery("from Catagory");
+			return query.list();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
